c50e339bfa677cdfda601cb195bed3d93f0fafba
Roman <roma.mereneanu@mail.ru>
2018-02-23 22:17:39 +0200

    Update 7.2

--- aletheme/options/admin/options-medialibrary-uploader.php
+++ aletheme/options/admin/options-medialibrary-uploader.php
@@ -211,8 +211,9 @@ if ( ! function_exists( 'optionsframework_mlu_get_silentpost' ) ) {
 			$_posts = $wpdb->get_row( $query );
 			
 			// If we've got a post, loop through and get it's ID.
-			if ( count( $_posts ) ) {
-				$_id = $_posts->ID;
+			if ( is_array( $_posts )) {
+				if ( count( $_posts ) )
+					$_id = $_posts->ID;
 			} else {
 			
 				// If no post is present, insert one.
